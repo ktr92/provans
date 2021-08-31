bf3e0f35f7af1d677ef7aa5d8fb8caba4e719540
ktr92 <kudashevtim@gmail.com>
2021-08-31 16:06:10 +0300

    start

--- js/main.js
+++ js/main.js
@@ -65,7 +65,14 @@ $(document).ready(function () {
       $("input[type=tel]").mask("+7 999 999 99 99")
 
 
-
+       jQuery("a.scrollTo").click(function() {
+        elementClick = jQuery(this).attr("href")
+        destination = jQuery(elementClick).offset().top - 90;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 1100);
+        return false;
+    });
 
 
 
